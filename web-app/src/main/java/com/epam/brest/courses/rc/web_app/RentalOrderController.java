b5c37061f22cbd198b2fd3464311a54f16028cec
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2019-03-29 21:50:48 +0300

    render rental orders list

--- web-app/src/main/java/com/epam/brest/courses/rc/web_app/RentalOrderController.java
+++ web-app/src/main/java/com/epam/brest/courses/rc/web_app/RentalOrderController.java
@@ -1,8 +1,14 @@
 package com.epam.brest.courses.rc.web_app;
 
+import com.epam.brest.courses.rc.service.RentalOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.stream.Collectors;
 
 /**
  * RentalOrder controller.
@@ -11,12 +17,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RentalOrderController {
 
     /**
+     * Logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(RentalOrderController.class);
+
+    /**
+     * Service.
+     */
+    @Autowired
+    private RentalOrderService orderService;
+
+    /**
      * Goto orders list page.
      *
+     * @param model rental orders DTO list
      * @return view name
      */
     @GetMapping(value = "/orders")
     public final String getOrdersPage(Model model) {
+        LOGGER.debug("findAll({})", model);
+        model.addAttribute("ordersDTO", orderService.findAllDTOs()
+                .collect(Collectors.toList()));
         return "orders";
     }
 
